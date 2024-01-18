8b8fb8e479605ca64c3bf1407201aa389083cf23
RasagnyaG <gudipudirasagnya@gmail.com>
2024-01-19 02:57:30 +0530

    consider file path

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ directory = sys.argv[1]
 
 for root, dirs, files in os.walk(directory):
         for file in files:
-            print(file)
             file_path = os.path.join(root, file)
-            with open(file, 'r') as file:
+            with open(file_path, 'r') as file:
                 data = file.read()
                 print(f"{file} =========> {data}")
