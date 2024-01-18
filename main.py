53a55aa0ba1b1c3a7e2f79fc68ceaacac285d551
RasagnyaG <gudipudirasagnya@gmail.com>
2024-01-19 02:54:21 +0530

    get file path

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 directory = sys.argv[1]
-print(directory)
 
 for root, dirs, files in os.walk(directory):
         for file in files:
+            print(file)
             file_path = os.path.join(root, file)
             with open(file, 'r') as file:
                 data = file.read()
